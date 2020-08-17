1d04fc71b6be408681d117053fe95c95afef6d16
PratikShah1 <68697498+PratikShah1@users.noreply.github.com>
2020-08-17 07:25:07 -0400

    Add files via upload

---
+++ FileModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace FolderBrowserCopy.Models
+{
+    public class FileModel
+    {
+        [Required(ErrorMessage = "Please select file.")]
+        [Display(Name = "Browse File")]
+        public HttpPostedFileBase[] Files { get; set; }
+    }
+}
